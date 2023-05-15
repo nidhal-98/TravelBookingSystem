b4239bfdff1122a842d24a96900cbd5f151e059a
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-15 11:29:05 +0400

    Packages are created

---
+++ src/test/java/com/example/Travel/booking/system/TravelBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Travel.booking.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
